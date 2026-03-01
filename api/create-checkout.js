77d5d0c0ce160928e64e1c8ee68361619796f21e
djm08215 <djm08215@gmail.com>
2026-03-02 05:20:37 +0900

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -7,7 +7,6 @@ const PACKAGES = {
 };
 
 export default async function handler(req, res) {
-  // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -28,7 +27,7 @@ export default async function handler(req, res) {
             name: `🪙 ${pkg.label}`,
             description: 'Coins for World Opinion Map',
           },
-          unit_amount: pkg.price, // cents
+          unit_amount: pkg.price,
         },
         quantity: 1,
       }],
